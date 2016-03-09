edd334b45cc04b3744e5452874cc3cc9a3d3d8a2
Bryan <Bpadron2000@gmail.com>
2016-03-08 22:13:52 -0500

    added event controller, views, db:seed

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,43 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+  def show
+    @event = Event.find(params[:id])
+  end
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
+  end
+  def new
+    @event = Event.new
+  end
+  def edit
+    @event = Event.find(params[:id])
+  end
+  def update
+    @event = Event.find(params[:id])
+      if @event.update_attributes(event_params)
+        redirect_to(:action => 'show', :id => @event.id)
+      else
+        render('edit')
+      end
+  end
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    if @event.destroy
+      redirect_to(:action => 'index')
+    else
+      render('delete')
+    end
+  end
+  private
+  def event_params
+    params.require(:event).permit(:name, :description)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307202741) do
+ActiveRecord::Schema.define(version: 20160308175208) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
